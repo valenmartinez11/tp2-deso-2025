635512aa1a677fb3ed8c43ad00f3da90cf130584
valentino <valentinomartinez11@hotmail.com>
2025-11-25 19:11:32 -0300

    feat: Initial commit and project setup (Maven/Spring Boot)

---
+++ src/main/java/com/reservas/hotel/api_gestion_hotelera/SistemaDeReservasHotelerasApplication.java
@@ -0,0 +1,13 @@
+package com.reservas.hotel.api_gestion_hotelera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeReservasHotelerasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeReservasHotelerasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reservas/hotel/api_gestion_hotelera/SistemaDeReservasHotelerasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservas.hotel.api_gestion_hotelera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeReservasHotelerasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
